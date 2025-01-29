54ad49f0118e561ced9ff28d61c9e1829f5d58b8
yoshbatula <yoshbatula2@gmail.com>
2025-01-29 11:27:12 +0800

    progress for php

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+</head>
+<body>
+        <form action="" method = "get">
+            <label for="fn">Email</label>
+            <input type="email" name="email">
+            <label for="">Password</label>
+            <input type="passowrd" name = "password">
+
+            <input type="submit" value = "submit">
+        </form>
+    
+    <?php 
+        echo "hello yosh"
+        $var1 = 5;
+        $var2 = 10;
+
+        echo "iloveyouu fletcher"
+        
+        $answer = $var1 + $var2;
+
+        echo "Value: " + $answer;
+
+        if
+
+    ?>
+</body>
+</html>
